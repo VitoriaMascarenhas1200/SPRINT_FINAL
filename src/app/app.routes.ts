86099aacc92ba4811b4eba61d4f2033d8bd4bad2
VitoriaMascarenhas1200 <vitoria.mascarenhas1200@gmail.com>
2026-07-14 19:01:26 -0300

    Adicionando guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { HomeComponent } from './pages/home/home';
 import { CadastroComponent } from './pages/cadastro/cadastro';
 import { LoginComponent } from './pages/login/login';
 import { MatchComponent } from './pages/match/match';
+import { authGuard } from './guard/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
   {path: 'cadastro', component: CadastroComponent},
-  { path: 'login', component: LoginComponent },
-  {path: 'match', component: MatchComponent},
+  {path: 'login', component: LoginComponent },
+  {path: 'match', component: MatchComponent, canActivate: [authGuard] },
 
   { path: '**', redirectTo: 'home' }
 ];
